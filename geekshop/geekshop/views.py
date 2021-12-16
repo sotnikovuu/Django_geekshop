16127c087cbbf214e7bf36b45652c1fdb10ae97f
sotnikovuu <sotnikovuu@yandex.ru>
2021-12-16 23:34:22 +0300

    Основы_Django_Framework__Практическое_задание_1

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'geekshop/index.html')
+    title = 'GeekShop'
+    context = {
+        'title' : title,
+    }
+    return render(request, 'geekshop/index.html', context)
 
 def contacts(request):
-    return render(request, 'geekshop/contact.html')
+    title = 'Контакты'
+    context = {
+        'title' : title,
+    }
+    return render(request, 'geekshop/contact.html', context)
